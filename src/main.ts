13255b6fdc185036398ab0d1db6f29bdaf9ff852
Christian <christian.biasi@hotmail.de>
2025-03-26 23:08:44 +0100

    deleted signle rendered box from beginning

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,6 @@ import Bird from "./bird";
 const a = new p5((sketch: p5) => {
     let x = 100;
     let y = 100;
-    let boxWidth = 50;
-    let boxHeight = 50;
     const canvasHeight = 400;
     const canvasWidth = 400;
     let bird = new Bird(sketch, x, y);
@@ -17,7 +15,6 @@ const a = new p5((sketch: p5) => {
     sketch.draw = () => {
         sketch.background(0);
         sketch.fill(255);
-        sketch.rect(x, y, boxWidth, boxHeight);
         bird.show();
         bird.update(canvasHeight);
     };
