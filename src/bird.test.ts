b4e34323a83ff6f82a4518e25824eef5971989c2
Christian <christian.biasi@hotmail.de>
2025-03-26 17:18:25 +0100

    adjusted tests after refactoring, less side effects

--- src/bird.test.ts
+++ src/bird.test.ts
@@ -5,8 +5,19 @@ test("If the bird hits the floor, its y position should be set to the height of 
     const bird = new Bird(undefined, 50, 50);
     const simulatedCanvasHeight = 50;
 
-    bird.update(simulatedCanvasHeight);
+    bird.stopIfOutOfBounds(simulatedCanvasHeight);
 
     expect(bird.y).toBe(simulatedCanvasHeight - 10);
+    expect(bird.velocity).toBe(0);
+});
+
+test("If the bird hits the ceiling, its y position should be set to the ceiling of the canvas + 10 and its velocity should be reset", () => {
+    const yPosition = -10;
+    const bird = new Bird(undefined, 50, yPosition);
+    const simulatedCanvasHeight = 50;
+
+    bird.stopIfOutOfBounds(simulatedCanvasHeight);
+
+    expect(bird.y).toBe(10);
     expect(bird.velocity).toBe(0);
 });
