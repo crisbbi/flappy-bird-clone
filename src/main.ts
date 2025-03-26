66e7403fa3f712e6d4cb74f501b4a382af7b42a5
Christian <christian.biasi@hotmail.de>
2025-03-26 23:09:47 +0100

    render pipes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import p5 from "p5";
 import Bird from "./bird";
+import Pipe from "./pipe";
 
 const a = new p5((sketch: p5) => {
     let x = 100;
@@ -7,6 +8,7 @@ const a = new p5((sketch: p5) => {
     const canvasHeight = 400;
     const canvasWidth = 400;
     let bird = new Bird(sketch, x, y);
+    let pipes: Pipe[] = [];
 
     sketch.setup = () => {
         sketch.createCanvas(canvasWidth, canvasHeight);
@@ -17,6 +19,18 @@ const a = new p5((sketch: p5) => {
         sketch.fill(255);
         bird.show();
         bird.update(canvasHeight);
+
+        if (sketch.frameCount % 100 === 0) {
+            pipes.push(new Pipe(sketch));
+        }
+        for (let index = pipes.length - 1; index >= 0; index--) {
+            pipes[index].show();
+            pipes[index].updatePipePosition();
+
+            if (pipes[index].x + pipes[index].width < -10) {
+                pipes.splice(index, 1);
+            }
+        }
     };
 
     sketch.keyPressed = () => {
